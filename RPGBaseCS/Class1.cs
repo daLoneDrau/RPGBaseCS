beb1353dc7ee97a0197fcabda1a30e83784b2866
daLoneDrau <donaldbrau@yahoo.com>
2017-10-03 23:17:04 -0400

    just an upload

---
+++ RPGBaseCS/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGBaseCS
+{
+    public class Class1
+    {
+    }
+}
